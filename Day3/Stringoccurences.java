813b497dd585df6bc838f104e5b0432d7827dea4
llyogesh123 <yogeshwaranjith2004@gmail.com>
2025-12-05 12:29:24 +0530

    File name changes

--- Day3/Stringoccurences.java
+++ Day3/Stringoccurences.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-class String {
+class Stringoccurences {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         String s1 = sc.nextLine();
