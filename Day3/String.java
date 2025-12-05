1d88354649e59eb35019a7f208bf3e79feef6300
llyogesh123 <yogeshwaranjith2004@gmail.com>
2025-12-05 11:50:14 +0530

    First program is completed

---
+++ Day3/String.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        int count = UserMainCode.countOccurances(s1, s2);
+        System.out.println(count);
+    }
+}
+
+class UserMainCode {
+    public static int countOccurances(String s1, String s2) {
+        int count = 0;
+        int index = 0;
+
+        while ((index = s1.indexOf(s2, index)) != -1) {
+            count++;
+            index = index + s2.length(); // move forward to avoid overlapping
+        }
+
+        return count;
+    }
+}
