25f22bdb95481ef18eb6ff8cb5552bfa03d3b8a4
llyogesh123 <yogeshwaranjith2004@gmail.com>
2025-12-10 12:21:28 +0530

    phone validator program

---
+++ Day6/PhoneValidator.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+
+class PhoneValidator{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		String inp=sc.nextLine();
+		System.out.println(UserMainCode.phoneValidator(inp));
+		
+	}
+}
+
+class UserMainCode{
+	public static boolean phoneValidator(String inp){
+		return inp.matches("[1-9]{2}[0-9]{8}");
+	}
+}
